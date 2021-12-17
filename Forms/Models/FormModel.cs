5814edb94da98e402a343e32d9d4767eaaa8dc16
pkaur <pkaur@LAPTOP-EDEI6SED>
2021-12-17 08:07:23 +0530

    Add project files.

---
+++ Forms/Models/FormModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forms.Models
+{
+    public class FormModel
+    {
+        [DataType(DataType.Date)]
+        public DateTime? date { get; set; }
+
+        public int numberOfDays { get; set; }
+    }
+}
